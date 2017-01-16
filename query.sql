dd6896e4ae5cba0d90626f3acc66e3c32008b520
mofemi <mofemi@gmail.com>
2017-01-16 18:37:30 +0100

    Added alias to the table field
    Added alias to the table field

--- query.sql
+++ query.sql
@@ -1,13 +1,13 @@
 SELECT 
-    Users.display_name,
-    Users.organization_name,
-    COUNT(DISTINCT contact.case_id) cases
+    Users.display_name AS "Contact Display Name",
+    Users.organization_name AS "Employer organisation name",
+    COUNT(DISTINCT contact.case_id)  AS "Number of cases"
 FROM
     civicrm_case_contact contact
         LEFT JOIN
     civicrm_case `case` ON (case.id = contact.case_id)
         LEFT JOIN
-    civicrm_contact users ON (users.id = contact.contact_id)
+    civicrm_contact Users ON (users.id = contact.contact_id)
 WHERE
     case.status_id = 1
 GROUP BY contact.contact_id;
